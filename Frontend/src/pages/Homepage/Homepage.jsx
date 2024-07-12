1e60772a2ba9aefec7fd1b07dd0a4325f67b7692
KBJ2 <KBJ2@LP15-KBJ2-IND>
2024-07-12 16:42:39 +0530

    More WebInfo

--- Frontend/src/pages/Homepage/Homepage.jsx
+++ Frontend/src/pages/Homepage/Homepage.jsx
@@ -1,21 +1,24 @@
 import Navbar from "../../Shared/Components/Header";
 import Footer from "../../Shared/Components/Footer";
 import QnA from "../QnA/QnA";
+import WebInfo from "../WebInfo/WebInfo";
 const Homepage = () => {
   return (
     <>
-    <div style={{
-      display:'flex',
-      flexDirection:'column',
-      height:'100vh',
-      width:'100%',
-      justifyContent:'space-between'
-    }}>
-
-      <Navbar />
-      <QnA/>
-      <Footer />
-    </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <Navbar />
+        <WebInfo />
+        <QnA />
+        <Footer />
+      </div>
     </>
   );
 };
