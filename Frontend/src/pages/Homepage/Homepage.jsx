203cc9f8741627db05b5a3b125f7ccfc96c03af7
KBJ2 <KBJ2@LP15-KBJ2-IND>
2024-07-14 13:57:13 +0530

    carousel first attempt

--- Frontend/src/pages/Homepage/Homepage.jsx
+++ Frontend/src/pages/Homepage/Homepage.jsx
@@ -1,21 +1,24 @@
 import Navbar from "../../Shared/Components/Header";
 import Footer from "../../Shared/Components/Footer";
 import QnA from "../QnA/QnA";
+import InfitneCarousel from "../../Shared/Components/infiniteCarousel/InfitneCarousel";
 const Homepage = () => {
   return (
     <>
-    <div style={{
-      display:'flex',
-      flexDirection:'column',
-      height:'100vh',
-      width:'100%',
-      justifyContent:'space-between'
-    }}>
-
-      <Navbar />
-      <QnA/>
-      <Footer />
-    </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <Navbar />
+        <InfitneCarousel />
+        <QnA />
+        <Footer />
+      </div>
     </>
   );
 };
