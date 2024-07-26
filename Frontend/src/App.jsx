211039e204f7301b3c300a910b538f0e83c4bfed
KBJ2 <KBJ2@LP15-KBJ2-IND>
2024-07-26 14:13:33 +0530

    Breadcrumbs component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import MyRoutes from "../Routes";
+import Breadcrumnbs from "./Shared/Components/Breadcrumbs";
 
 function App() {
   return (
     <>
       <Router>
         <MyRoutes />
+        <Breadcrumnbs/>
       </Router>
     </>
   );
