7ee8027c3f1c4d54ee1b01022c649e548c15af21
KBJ2 <KBJ2@LP15-KBJ2-IND>
2024-07-30 12:49:24 +0530

    progress bar using Css [Fix the navbar for this to work]

--- Frontend/src/Shared/Components/Header.jsx
+++ Frontend/src/Shared/Components/Header.jsx
@@ -15,6 +15,8 @@ import LocalMallIcon from "@mui/icons-material/LocalMall";
 import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShoppingCartCheckoutSharpIcon from "@mui/icons-material/ShoppingCartCheckoutSharp";
+import { useEffect } from "react";
+import '../Styles/HeaderStyle.css'
 const pages = ["Products", "Blog", "About Us", "Contact Us"];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 // const iconData = [
@@ -48,111 +50,136 @@ function ResponsiveAppBar() {
     setAnchorElUser(null);
   };
 
-  return (
-    <AppBar style={{ backgroundColor: "#1b2833" }} position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <LocalMallIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            // href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            CoolName+
-          </Typography>
+  useEffect(() => {
+    window.onscroll = function () {
+      myFunction();
+    };
 
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            // href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Box>
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <div style={{ marginTop: "10px" }}>
-                <Tooltip title="Search">
-                  <SearchSharpIcon
-                    style={{ marginRight: "20px", color: "white" }}
-                    alt="search"
-                  />
-                </Tooltip>
-                <Tooltip title="Wishlist">
-                  <FavoriteIcon
-                    style={{ marginRight: "20px", color: "white" }}
-                    alt="wishlist"
-                  />
-                </Tooltip>
-                <Tooltip title="Checkout">
-                  <ShoppingCartCheckoutSharpIcon
-                    style={{ marginRight: "20px", color: "white" }}
-                    alt="checkout"
-                  />
-                </Tooltip>
-              </div>
-              <Tooltip title="Settings">
-                <Avatar alt="Remy Sharp" />
-              </Tooltip>
-            </IconButton>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
+    function myFunction() {
+      var winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
+      var height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("myBar").style.width = scrolled + "%";
+    }
+  }, []);
+  return (
+    <>
+      <AppBar style={{ backgroundColor: "#1b2833" }} position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <LocalMallIcon
+              sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+            />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              // href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
+              CoolName+
+            </Typography>
+
+            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              // href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box
+              sx={{ flexGrow: 1, display: "flex", justifyContent: "center" }}
+            >
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            </Box>
+            <Box>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <div style={{ marginTop: "10px" }}>
+                  <Tooltip title="Search">
+                    <SearchSharpIcon
+                      style={{ marginRight: "20px", color: "white" }}
+                      alt="search"
+                    />
+                  </Tooltip>
+                  <Tooltip title="Wishlist">
+                    <FavoriteIcon
+                      style={{ marginRight: "20px", color: "white" }}
+                      alt="wishlist"
+                    />
+                  </Tooltip>
+                  <Tooltip title="Checkout">
+                    <ShoppingCartCheckoutSharpIcon
+                      style={{ marginRight: "20px", color: "white" }}
+                      alt="checkout"
+                    />
+                  </Tooltip>
+                </div>
+                <Tooltip title="Settings">
+                  <Avatar alt="Remy Sharp" />
+                </Tooltip>
+              </IconButton>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      <div className="progress-container">
+        <div className="progress-bar" id="myBar"></div>
+      </div>
+      </AppBar>
+    </>
   );
 }
+
 export default ResponsiveAppBar;
