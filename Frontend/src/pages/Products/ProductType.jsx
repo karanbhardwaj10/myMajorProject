7284460e99b6694e58f6f3f865831f9ea047614b
KBJ2 <KBJ2@LP15-KBJ2-IND>
2024-08-18 20:58:45 +0530

    minor fix , added box for each card

--- Frontend/src/pages/Products/ProductType.jsx
+++ Frontend/src/pages/Products/ProductType.jsx
@@ -1,5 +1,5 @@
 import CustomProductView from "../../Shared/Components/customProductView/CustomProductView";
-import { Grid, Container } from "@mui/material";
+import { Grid, Container, Box } from "@mui/material";
 import test8 from "../../assets/test8.jpg";
 import test11 from "../../assets/test11.jpg";
 import test12 from "../../assets/test12.jpg";
@@ -12,34 +12,34 @@ const ProductType = () => {
       price: "Rs. " + 1999 + "*",
       path: "/men",
       discountPercentage: "Heavy Discounts",
-      isProduct:true,
+      isProduct: true,
     },
     {
       title: "W o m e n",
       imgVal: test11,
-      price:  "Rs. " + 2999 + "*",
+      price: "Rs. " + 2999 + "*",
       path: "/women",
       discountPercentage: "Heavy Discounts",
     },
     {
       title: "Coming Soon",
       imgVal: test12,
-      price:  "Rs. " + 3999 + "*",
+      price: "Rs. " + 3999 + "*",
       path: "/coming-soon",
       discountPercentage: "Heavy Discounts",
     },
   ];
   console.log(products[1].imgVal);
   return (
-    <Container style={{ marginLeft: "250px" }}>
+    <Container  style={{ marginLeft: "250px" }}>
       {/* <h1>Product Type</h1> */}
-      <Grid container spacing={15}>
+      <Grid  container spacing={15} marginTop={2}>
         {products.map((product, index) => (
           <Grid
             display={"flex"}
-            justifyContent={"space-between"}
-            
-            style={{ paddingLeft: "0px" }}
+            justifyContent={"start"}
+            // flexDirection={"column"}
+            style={{ padding: "0px" }}
             item
             xs={12}
             sm={7}
@@ -47,18 +47,20 @@ const ProductType = () => {
             lg={4}
             key={index}
           >
-            <Link
-              to={product.path}
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <CustomProductView
-                discountPercentage={product.discountPercentage}
-                title={product.title}
-                imgVal={product.imgVal}
-                price={product.price}
-                isProduct={product.isProduct}
-              />
-            </Link>
+            <Box>
+              <Link
+                to={product.path}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                <CustomProductView
+                  discountPercentage={product.discountPercentage}
+                  title={product.title}
+                  imgVal={product.imgVal}
+                  price={product.price}
+                  isProduct={product.isProduct}
+                />
+              </Link>
+            </Box>
           </Grid>
         ))}
       </Grid>
